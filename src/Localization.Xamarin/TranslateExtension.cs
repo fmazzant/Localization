b32a948c3bf63cfa909b05d51658cba72b66068e
Federico Mazzanti <f.mazzanti@vargroup.it>
2020-11-06 15:12:49 +0100

    created a base project

---
+++ src/Localization.Xamarin/TranslateExtension.cs
@@ -0,0 +1,64 @@
+﻿// <summary>
+/// 
+/// The MIT License (MIT)
+/// 
+/// Copyright (c) 2020 Federico Mazzanti
+/// 
+/// Permission is hereby granted, free of charge, to any person
+/// obtaining a copy of this software and associated documentation
+/// files (the "Software"), to deal in the Software without
+/// restriction, including without limitation the rights to use,
+/// copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following
+/// conditions:
+/// 
+/// The above copyright notice and this permission notice shall be
+/// included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+/// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+/// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+/// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+/// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+/// OTHER DEALINGS IN THE SOFTWARE.
+/// 
+/// </summary>
+
+namespace Xamarin.Localization
+{
+    using global::Localization;
+    using global::Localization.Xamarin.Converts;
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension<BindingBase>
+    {
+        public string Text { get; set; }
+        public string StringFormat { get; set; }
+        public string DefaultValue { get; set; }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+
+        public BindingBase ProvideValue(IServiceProvider serviceProvider)
+        {
+            var binding = new Binding
+            {
+                Mode = BindingMode.OneWay,
+                Path = $"[{Text}]",
+                Source = LocalizationManager.Instance,
+                StringFormat = StringFormat,
+                Converter = new NullToDefaultConverter(),
+                ConverterParameter = DefaultValue
+            };
+            return binding;
+        }
+    }
+}

5c16f484f4da4cc779806d47c7989b99d2996979
Federico Mazzanti <f.mazzanti@vargroup.it>
2020-11-09 13:09:26 +0100

    added the Localization.Windows project to manage the culture with WPF tecnology

---
+++ src/Localization.Windows/TranslateExtention.cs
@@ -0,0 +1,69 @@
+﻿// <summary>
+/// 
+/// The MIT License (MIT)
+/// 
+/// Copyright (c) 2020 Federico Mazzanti
+/// 
+/// Permission is hereby granted, free of charge, to any person
+/// obtaining a copy of this software and associated documentation
+/// files (the "Software"), to deal in the Software without
+/// restriction, including without limitation the rights to use,
+/// copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following
+/// conditions:
+/// 
+/// The above copyright notice and this permission notice shall be
+/// included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+/// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+/// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+/// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+/// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+/// OTHER DEALINGS IN THE SOFTWARE.
+/// 
+/// </summary>
+
+namespace Localization.Windows
+{
+    using Localization.Windows.Converters;
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Markup;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [ContentProperty(nameof(Text))]
+    public class TranslateExtension : MarkupExtension
+    {
+        public TranslateExtension() { }
+
+        public TranslateExtension(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; set; }
+
+        public string StringFormat { get; set; }
+        public string DefaultValue { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var binding = new Binding()
+            {
+                Source = LocalizationManager.Instance,
+                Path = new PropertyPath($"[{Text}]"),
+                Converter = new NullToDefaultConverter(),
+                ConverterParameter = DefaultValue,
+                StringFormat = StringFormat
+            };
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+}

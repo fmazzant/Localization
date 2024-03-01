48921133b49aaccb4a0e21a94fbc1738b8163e76
Federico Mazzanti <f.mazzanti@adiacent.com>
2024-03-01 17:22:16 +0100

    first commit

---
+++ samples/Localization.Samples.AppMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Localization.Samples.AppMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
